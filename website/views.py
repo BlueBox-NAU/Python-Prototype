b0075e3832bd523aadf4de8a49f97e6d07daa1f0
zacharwl123 <zsw23@nau.edu>
2021-02-01 23:17:55 -0700

    tweaks

--- website/views.py
+++ website/views.py
@@ -15,7 +15,7 @@ membersVar = [
 def home(request):
     return {}
 
-#Actually a sign up
+#SIGN IN
 @view_config(route_name='demo1', renderer='demo1/index.html')
 def demo1(request):
     if request.method == 'POST':
@@ -30,7 +30,7 @@ def demo1(request):
     else:
         return{'members': membersVar} # display members in membersVar at bottom of page
 
-# Actually a sign up
+#SIGN UP
 @view_config(route_name='register', renderer='demo1/register.html')
 def register(request):
     if request.method == 'POST':
@@ -41,20 +41,3 @@ def register(request):
         return HTTPFound(location=url)
     else:
         return{'members': membersVar} # display members in membersVar at bottom of page
-    
-
-
-# Actually a sign up
-# @view_config(route_name='signup', renderer='demo1.html')
-# def demo1(request):
-#     if request.method == 'POST':
-#         print("BEFORE\n==========\n", members, "\n")
-#         members.append({'id': 99, 
-#                         'email':request.params['email'],
-#                         'password':request.params['password']})
-#         print("AFTER\n==========\n", members, "\n")
-#         return{}
-#     else:
-#         return{}
-
-
